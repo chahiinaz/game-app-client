abcf9c73cfa6b8c87d7ebb4180f23987f4f65198
chahiinaz <chahiinaz@gmail.com>
2019-12-18 15:15:14 +0100

    lobby fixed, can click on names redirected to room. Room shows id and name

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class LobbyList extends Component {
+  url = "http://localhost:4000";
+  stream = new EventSource(
+    `${this.url}/gameroom/${this.props.match.params.id}`
+  );
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      const { data } = event;
+      const action = JSON.parse(data);
+      this.props.dispatch(action);
+      console.log("action", action);
+    };
+  }
+
   render() {
     const title = "Do you wanna play a game?";
     const { gameRooms } = this.props;
@@ -14,6 +28,7 @@ class LobbyList extends Component {
             <div key={index}>
               <h2>Gameroom name: {gameroom.name}</h2>
               <h1>id: {gameroom.id}</h1>
+              <button>click to join room</button>
             </div>
           );
         })}
@@ -23,7 +38,7 @@ class LobbyList extends Component {
 }
 
 function mapStateToProps(reduxstate) {
-  // console.log("reduxState", reduxState);
+  console.log("reduxState gameroom", reduxstate);
   return {
     gameRooms: reduxstate.lobbyReducer
   };
