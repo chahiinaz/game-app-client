084e9cbe595ed0e0a6fb3dbcfc888052e5d41d29
chahiinaz <chahiinaz@gmail.com>
2019-12-20 11:11:46 +0100

    added onclick to choice

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -3,28 +3,45 @@ import superagent from "superagent";
 import { connect } from "react-redux";
 
 class Game extends Component {
-  onClick = async gameroomId => {
-    try {
-      const response = await superagent.put(`${this.joinUrl}/choice`).send({
-        gameroomId,
-        userId: this.props.user.id,
-        choice: this.props.choice
-      });
+  url = `http://localhost:4000`;
 
-      console.log("response test: ", response);
+  onClickDefect = async event => {
+    try {
+      event.persist();
+      const response = await superagent.put(`${this.url}/choice`).send({
+        userId: this.props.user.id,
+        choice: false
+      });
+      console.log("response test1: ", response);
     } catch (error) {
       console.log(error);
     }
   };
+
+  onClickCooperate = async event => {
+    try {
+      event.persist();
+      const response = await superagent.put(`${this.url}/choice`).send({
+        userId: this.props.user.id,
+        choice: true
+      });
+
+      console.log("response test2: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <div>
-        <button>Defect</button>
-        <button>Cooperate</button>
+        <button onClick={this.onClickDefect}>Defect</button>
+        <button onClick={this.onClickCooperate}>Cooperate</button>
       </div>
     );
   }
 }
+
 function mapStateToProps(reduxstate) {
   console.log("reduxState choice", reduxstate);
   return {
